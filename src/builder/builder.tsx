286ff8fc1410bae611dc28c819bbd618d2172944
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-06-24 22:34:39 +0200

    server side rendering

--- src/builder/builder.tsx
+++ src/builder/builder.tsx
@@ -1,7 +1,18 @@
+/* eslint-disable no-await-in-loop */
 /* eslint-disable no-console */
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { inspect } from 'util';
+
+import path from 'path';
+import {
+  readFile,
+  writeFile,
+  readdir,
+  stat,
+  mkdir,
+  unlink,
+} from 'fs/promises';
 
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
@@ -67,14 +78,83 @@ const extractAllLinks = () => {
   return [...allLinks.values()];
 };
 
-const allLinks = extractAllLinks();
+const main = async () => {
+  const allLinks = extractAllLinks();
 
-console.log(`Found links: ${allLinks.map((l) => `"${l}"`).join(', ')}.`);
+  console.log(`Found links: ${allLinks.map((l) => `"${l}"`).join(', ')}.`);
 
-console.log('Now generating the static pages...');
-for (const link of allLinks) {
-  console.log(`Processing ${link}`);
-  const [app] = createAppForURL(link);
-  const markup = ReactDOMServer.renderToString(app);
-  console.log(markup);
-}
+  const indexBuffer = await readFile(path.resolve(__dirname, 'index.template.html'));
+  const indexTemplate = indexBuffer.toString();
+
+  const docsRootPath = path.resolve(__dirname, '..', '..', 'docs');
+  const appPlaceholder = '#APP#';
+
+  const createDocPath = async (link: string): Promise<string> => {
+    if (link === '/') {
+      return path.join(docsRootPath, 'index.html');
+    }
+
+    const linkParts = link.split('/');
+    const fileBasename = linkParts.pop();
+
+    const dirs = [];
+
+    for (const part of linkParts) {
+      dirs.push(part);
+      const dirPath = path.join(docsRootPath, ...dirs);
+      try {
+        await stat(dirPath);
+      } catch (err) {
+        if (err.code !== 'ENOENT') {
+          throw err;
+        }
+        mkdir(dirPath);
+      }
+    }
+
+    return path.join(docsRootPath, ...dirs, `${fileBasename}.html`);
+  };
+
+  const cleanupDocs = async (dirPath: string, currentDepth = 0): Promise<void> => {
+    const entries = await readdir(dirPath);
+    const kept = [];
+
+    const rm = async (p: string): Promise<void> => {
+      console.log(`Unlinking "${p}"...`);
+      await unlink(p);
+    };
+
+    for (const entry of entries) {
+      const entryPath = path.join(dirPath, entry);
+      const s = await stat(entryPath);
+      if (s.isDirectory()) {
+        await cleanupDocs(entryPath, currentDepth + 1);
+        await rm(entryPath);
+      } else if (/\.html$/.test(entry)) {
+        await rm(entryPath);
+      } else {
+        kept.push(entryPath);
+      }
+    }
+    if (kept.length === 0 && currentDepth > 0) {
+      await rm(dirPath);
+    }
+  };
+
+  console.log('Cleaning up previously generated pages...');
+  await cleanupDocs(docsRootPath);
+
+  console.log('Now generating the static pages...');
+  for (const link of allLinks) {
+    console.log(`Processing "${link}"...`);
+    const [app] = createAppForURL(link);
+    const markup = ReactDOMServer.renderToString(app);
+    console.log(markup);
+
+    const docPath = await createDocPath(link);
+    console.log(`Writing template file "${docPath}"...`);
+    await writeFile(docPath, indexTemplate.replace(appPlaceholder, markup));
+  }
+};
+
+main();
