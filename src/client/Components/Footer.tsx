5250d6b9bd3778bde8542e60f68f69422fdbb89e
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-06-27 16:24:36 +0200

    added a footer

---
+++ src/client/Components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  const markup = (
+    <div>
+      © François-Marie de Jouvencel<br />
+      <a href="mailto:fm.de.jouvencel@gmail.com">fm.de.jouvencel@gmail.com</a>
+    </div>
+  );
+
+  return markup;
+};
+
+export default Footer;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Router from './Components/Router';
+import Footer from './Components/Footer';
 
 render(<Router />, document.getElementById('root'));
+render(<Footer />, document.getElementById('footer'));
