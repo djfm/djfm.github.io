d05ade6e156ccd5549dd4f17d0d6fbd659a3685c
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-06-28 18:15:25 +0200

    automatically readjust code sample after 2s

--- src/client/Components/common/CodeSample.tsx
+++ src/client/Components/common/CodeSample.tsx
@@ -2,7 +2,6 @@ import React, {
   useState,
   useEffect,
   useRef,
-  BaseSyntheticEvent,
 } from 'react';
 
 import styled from 'styled-components';
@@ -10,43 +9,6 @@ import styled from 'styled-components';
 import {
   darkColor,
 } from './Styled';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  margin-bottom: 20px;
-
-  > figure {
-    max-width: 100%;
-
-    > figcaption {
-      max-width: 100%;
-      margin: 0 auto;
-    }
-
-    pre {
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-
-      max-width: 100%;
-      overflow: auto;
-
-      background-color: ${darkColor};
-
-      code {
-        width: fit-content;
-      }
-    }
-
-    margin-left: 0;
-    margin-right: 0;
-    margin-bottom: 0;
-  }
-`;
 
 const trimLeadingWhitespace = (input: unknown): typeof input => {
   if (typeof input !== 'string') {
@@ -93,11 +55,45 @@ const parseValueWithUnit = (val: string): [number, string] => {
   return [parseFloat(value), unit];
 };
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-bottom: 20px;
+
+  > figure {
+    max-width: 100%;
+
+    > figcaption {
+      max-width: 100%;
+      margin: 0 auto;
+    }
+
+    pre {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      max-width: 100%;
+      overflow: auto;
+
+      code {
+        padding: 2px;
+      }
+    }
+
+    margin-left: 0;
+    margin-right: 0;
+    margin-bottom: 0;
+  }
+`;
+
 export const CodeSample: React.FC<{
   title: string,
   children: unknown,
   language?: string,
-  childIsPre?: boolean,
 }> = ({
   title,
   children,
@@ -106,7 +102,12 @@ export const CodeSample: React.FC<{
   const minZoom = 10;
   const normalZoom = 100;
   const maxZoom = 170;
+  const readjustDelay = 2000;
 
+  const [isZooming, setIsZooming] = useState(false);
+  const [readjustNeeded, setReadjustNeeded] = useState(false);
+  const [heightWasFixed, setHeightWasFixed] = useState(false);
+  const [latestZoomOperationTime, setLatestZoomOperationTime] = useState(0);
   const [zoomLevel, setZoomLevel] = useState(normalZoom);
   const [initialFontSize, setInitialFontSize] = useState(0);
   const [fontUnit, setFontUnit] = useState('');
@@ -118,27 +119,68 @@ export const CodeSample: React.FC<{
     // necessary to prevent errors in the SSR code where
     // document is not defined
     if (typeof document !== 'undefined') {
+      if (heightWasFixed || !isZooming) {
+        return;
+      }
+
       const codeStyle = document.defaultView.getComputedStyle(
         codeElementRef.current,
       );
 
-      const { fontSize } = codeStyle;
-      const [domInitialFontSize, domFontUnit] = parseValueWithUnit(fontSize);
-      setInitialFontSize(domInitialFontSize);
-      setFontUnit(domFontUnit);
+      if (initialFontSize === 0) {
+        const { fontSize } = codeStyle;
+        const [domInitialFontSize, domFontUnit] = parseValueWithUnit(fontSize);
+        setInitialFontSize(domInitialFontSize);
+        setFontUnit(domFontUnit);
+      }
 
       const { height } = document.defaultView.getComputedStyle(preElementRef.current);
-      const [initialPreHeight, preHeightUnit] = parseValueWithUnit(height);
-      const fixedPreHeight = `${initialPreHeight * 1.5}${preHeightUnit}`;
 
-      preElementRef.current.style.height = fixedPreHeight;
+      preElementRef.current.style.minHeight = height;
+      setHeightWasFixed(true);
     }
-  }, []);
+  }, [isZooming, heightWasFixed, initialFontSize]);
 
-  const updateZoomLevel = (e: BaseSyntheticEvent) => {
+  useEffect(() => {
+    if (readjustNeeded) {
+      readjust();
+      setReadjustNeeded(false);
+    }
+  }, [readjustNeeded]);
+
+  useEffect(() => {
+    if (latestZoomOperationTime === 0) {
+      return undefined;
+    }
+
+    const h = setTimeout(() => {
+      if (Date.now() > latestZoomOperationTime + readjustDelay) {
+        stopZooming();
+      }
+    }, readjustDelay);
+
+    return () => {
+      clearTimeout(h);
+    };
+  }, [latestZoomOperationTime]);
+
+  const readjust = () => {
+    preElementRef.current.style.minHeight = 'auto';
+  };
+
+  const handleZoomInputChange = (e: React.BaseSyntheticEvent) => {
     const { value: valueString } = e.target;
     const value = parseInt(valueString, 10);
     setZoomLevel(value);
+    setIsZooming(true);
+    setLatestZoomOperationTime(Date.now());
+  };
+
+  const stopZooming = () => {
+    setIsZooming(false);
+    setReadjustNeeded(true);
+    setHeightWasFixed(false);
+    setLatestZoomOperationTime(0);
   };
 
   const scaleFactor = zoomLevel / normalZoom;
@@ -177,7 +219,7 @@ export const CodeSample: React.FC<{
           min={minZoom}
           max={maxZoom}
           value={zoomLevel}
-          onChange={updateZoomLevel}
+          onChange={handleZoomInputChange}
         />
       </label>
     </Wrapper>
