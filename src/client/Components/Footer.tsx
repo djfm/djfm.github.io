d398c75dc6264a9db130aadccb25c36af2b02ea7
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-07-04 22:53:58 +0200

    markup cleanup

--- src/client/Components/Footer.tsx
+++ src/client/Components/Footer.tsx
@@ -2,30 +2,19 @@ import React from 'react';
 
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+const FooterWrapper = styled.div`
   margin-top: 50px;
   font-size: 0.7em;
   text-align: right;
-
-  > div {
-    display: inline-block;
-    text-align: center;
-    margin-right: 10px;
-    padding-bottom: 5px;
-  }
+  padding-bottom: 10px;
+  margin-right: 10px;
 `;
 
-export const Footer: React.FC = () => {
-  const markup = (
-    <Wrapper>
-      <div>
-        © François-Marie de Jouvencel<br />
-        <a href="mailto:fm.de.jouvencel@gmail.com">fm.de.jouvencel@gmail.com</a>
-      </div>
-    </Wrapper>
-  );
-
-  return markup;
-};
+export const Footer: React.FC = () => (
+  <FooterWrapper>
+    © François-Marie de Jouvencel<br />
+    <a href="mailto:fm.de.jouvencel@gmail.com">fm.de.jouvencel@gmail.com</a>
+  </FooterWrapper>
+);
 
 export default Footer;
