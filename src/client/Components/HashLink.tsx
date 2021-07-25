512c4ef19fdc65c624b49a08cfd7a6a9a044b504
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-07-25 11:24:12 +0200

    removed console log & useless import

--- src/client/Components/HashLink.tsx
+++ src/client/Components/HashLink.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-import {
-  useLocation,
-} from 'react-router-dom';
-
 export type HashLinkProps = React.PropsWithChildren<{
   anchor: string
 }>
@@ -11,11 +7,6 @@ export type HashLinkProps = React.PropsWithChildren<{
 export const HashLink: React.FC<HashLinkProps> = ({
   anchor,
   children,
-}: HashLinkProps) => {
-  const { pathname } = useLocation();
-  console.log(pathname);
-
-  return <a href={`#${anchor}`}>{children}</a>;
-};
+}: HashLinkProps) => <a href={`#${anchor}`}>{children}</a>;
 
 export default HashLink;
