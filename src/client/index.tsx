9972e6bb2d74432d1f4ca3563215bb89ae2e22e6
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-07-28 14:35:35 +0200

    moved  serviceWorker install to index.html tpl

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -8,18 +8,3 @@ import Router from './Components/Router';
 const show = process.env.NODE_ENV === 'development' ? render : hydrate;
 
 show(<Router />, document.getElementById('root'));
-
-if (typeof navigator !== 'undefined') {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', async () => {
-      try {
-        await navigator
-          .serviceWorker
-          .register('/worker.js');
-        // console.log('Service Worker Registered', { reg });
-      } catch (e) {
-        console.error('Service Worker failed to register', e);
-      }
-    });
-  }
-}
