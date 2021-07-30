29e4ad4fb47015ab595f229723004b5366ce503f
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-07-30 07:28:20 +0200

    removed outdated comment

--- src/client/Components/Router.tsx
+++ src/client/Components/Router.tsx
@@ -7,32 +7,6 @@ import {
 
 import App from './App';
 
-/*
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    const depth = pathname.split('/').length - 1;
-    if (typeof window !== 'undefined') {
-      if (depth === 1) {
-        window.scrollTo(0, 0);
-      } else {
-        const [heading] = document.getElementsByTagName(`h${depth}`);
-        if (heading) {
-          const tooLow = heading.getBoundingClientRect().y > window.screen.height - 100;
-          const tooHigh = heading.getBoundingClientRect().y < 0;
-          if (tooLow || tooHigh) {
-            heading.scrollIntoView(true);
-          }
-        }
-      }
-    }
-  });
-
-  return null;
-};
-*/
-
 export const Router: React.FC = () => (
   <BrowserRouter>
     <App />
