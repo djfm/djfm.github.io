22d4fd3f936b99aad8726e68f6f9615368c5aa1b
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-08-03 00:03:49 +0200

    imrpoved the definition of md docs

--- src/@types/md.d.ts
+++ src/@types/md.d.ts
@@ -1,8 +1,23 @@
+type ParserNodeType =
+  'document'
+  | 'section'
+  | 'heading-1'
+  | 'heading-2'
+  | 'literal'
+  | 'paragraph'
+  | 'bold'
+  | 'idiomatic'
+  | 'blockquote'
+  | 'list'
+  | 'list-item'
+  | 'function-call'
+  | 'function-call-arg'
+
 type ParserNode = {
-  type: string;
+  type: ParserNodeType
   value?: string;
   children?: ParserNode[]
-  props?: Record<string, string>
+  props?: Record<string, string | number>
 }
 
 declare module '*.md' {
