8909545691e4e8a394a9539cb388084e34d25bc3
François-Marie de Jouvencel <fm.de.jouvencel@gmail.com>
2021-08-02 15:49:26 +0200

    just a test script to better see what I'm doing

--- src/MDParser/tmp.ts
+++ src/MDParser/tmp.ts
@@ -1,6 +1,12 @@
+import util from 'util';
 import path from 'path';
 import { readFile } from 'fs/promises';
 import { parser } from './parser';
+
+// eslint-disable-next-line no-console
+const inspect = (obj: unknown) => console.log(
+  util.inspect(obj, false, null, true),
+);
 
 const main = async () => {
   const rootDir = path.resolve(__dirname, '../Pages');
@@ -8,8 +14,7 @@ const main = async () => {
   const testFileBuf = await readFile(path.join(rootDir, 'Home.md'));
   const testFile = testFileBuf.toString();
   const parsed = await parser(testFile, rootDir);
-
-  console.log(parsed);
+  inspect(parsed);
 };
 
 main();
